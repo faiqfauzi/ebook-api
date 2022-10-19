5212762be702db2cef8aa349eddcf31a5ae08d00
unknown <faiqfauzi101@gmail.com>
2022-10-19 14:27:09 +0700

    endpoint siswa

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\SiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {//
+//    return $request->user();//
+//});//
+
+/*
+*Task
+*URL : http://localhost:8000/api/halo
+*Method: GET
+*Exec: function
+*Return: JSON=> {"me": "contoh"}
+
+*/
+
+Route::get('halo', function(){
+    $data = ["me" => "contoh"];
+
+    return $data;
 });
+
+
+Route::resource('hellocontroller', HelloController::class);
+Route::resource('siswa', SiswaController::class);
