3df6700a66cdf75bf3665043f1f29d4beb7c3dce
unknown <faiqfauzi101@gmail.com>
2022-10-31 11:13:54 +0700

    sanctum

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,3 +41,20 @@ Route::get('halo', function(){
 Route::resource('hellocontroller', HelloController::class);
 Route::resource('siswa', SiswaController::class);
 Route::resource('book', BookController::class);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request){
+    return $request->user();
+});
+
+//public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/Books/{id}', [BookController::class, 'show']);
+
+//protected routes
+Route::middleware('auth:sanctum')->group(function (){
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
